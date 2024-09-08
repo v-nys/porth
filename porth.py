8f6589053067aa358913bf9cc6a1f0667beaca97
Vincent Nys <vincentnys@gmail.com>
2024-09-08 09:42:57 +0200

    Met reset functionaliteit.

--- porth.py
+++ porth.py
@@ -2,13 +2,15 @@
 
 counter = 0
 
-def iota():
+def iota(reset=False):
     global counter
+    if reset:
+        counter = 0
     current_value = counter
     counter += 1
     return current_value
 
-OP_PUSH = iota()
+OP_PUSH = iota(reset=True)
 OP_PLUS = iota()
 OP_DUMP = iota()
 COUNT_OPS = iota()
